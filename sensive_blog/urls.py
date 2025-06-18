2bf093f47cf42bbd920a1d74a90dc2efb2c519da
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-06-18 23:06:59 +0500

    Add index page layout

--- sensive_blog/urls.py
+++ sensive_blog/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+import os
 
 from blog import views
 
@@ -15,6 +16,10 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
+
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'static'))
+    # или
+    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
